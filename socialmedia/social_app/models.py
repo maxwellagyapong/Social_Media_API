d94dcb36c639697a4ee77158a400cf5bb8e67ccc
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-24 20:51:44 +0100

    Defined upload to path for UserPost media_file field

--- socialmedia/social_app/models.py
+++ socialmedia/social_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserPost(models.Model):
     post_owner = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=250)
-    media_file = models.FileField(upload_to='', blank=True)
+    media_file = models.FileField(upload_to='media/posts-media', blank=True)
     date_posted = models.DateTimeField(auto_now=True)
     comments_count = models.IntegerField(default=0)
     likes_count = models.IntegerField(default=0)
