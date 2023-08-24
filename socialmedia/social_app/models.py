c74629a14ff29afaa92971e640a6f259de67d940
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-24 20:23:34 +0100

    Added UserPost model

--- socialmedia/social_app/models.py
+++ socialmedia/social_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserPost(models.Model):
+    post_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    media_file = models.FileField(upload_to='')
+    date_posted = models.DateTimeField(auto_now=True)
+    comments_count = models.IntegerField(default=0)
+    likes_count = models.IntegerField(default=0)
+    
