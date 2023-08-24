b8bbd1e018c5b889d04675a6ce7f2ca84c8538a2
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-25 00:21:40 +0100

    Created group serializer

--- socialmedia/social_app/serializers.py
+++ socialmedia/social_app/serializers.py
@@ -15,24 +15,25 @@ class CommentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Commment
-        exclude = ('replies_count', 'parent_post',)
+        exclude = ('parent_post',)
 
 
-class UserPostSerializer(serializers.ModelSerializer):
-    post_owner = serializers.StringRelatedField(read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = UserPost
-        exclude = ('comments_count', 'likes_count',)
-        
-        
 class LikeSerializer(serializers.ModelSerializer):
     liker = serializers.StringRelatedField(read_only=True)
     
     class Meta:
         model = Like
         exclude = ('parent_post',)
+
+
+class UserPostSerializer(serializers.ModelSerializer):
+    post_owner = serializers.StringRelatedField(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    # likes = LikeSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = UserPost
+        fields = '__all__'
         
         
 class GroupMemberSerializer(serializers.ModelSerializer):
@@ -43,7 +44,13 @@ class GroupMemberSerializer(serializers.ModelSerializer):
         exclude = ('group_joined',)
         
         
-
+class GroupSerializer(serializers.ModelSerializer):
+    owner = serializers.StringRelatedField(read_only=True) 
+    # group_member = GroupMemberSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
         
