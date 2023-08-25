abad7e649ab6e365c012f991bbc27e3fb5041332
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-25 11:39:35 +0100

    Created enpoint for post detail

--- socialmedia/social_app/urls.py
+++ socialmedia/social_app/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('home/', PostListGV.as_view(), name='home'),
     path('post/', CreatePostGV.as_view(), name='create-post'),
+    path('post/<int:pk>/', PostDetailGV.as_view(), name='post-detail'),
 ]
