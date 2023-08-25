6cfb41cd76bea9ed5c51b719134130fe8b7ae3f6
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-25 12:25:34 +0100

    Created view for liking and unliking posts

--- socialmedia/social_app/views.py
+++ socialmedia/social_app/views.py
@@ -24,4 +24,29 @@ class PostListGV(generics.ListAPIView):
 class PostDetailGV(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserPost.objects.all()
     serializer_class = UserPostSerializer
-    permission_classes = [IsAuthenticated, IsPostOwnerOrReadOnly]    
+    permission_classes = [IsAuthenticated, IsPostOwnerOrReadOnly]
+    
+
+class LikeAndUnlikePostGV(generics.CreateAPIView):
+    serializer_class = LikeSerializer
+    
+    def get_queryset(self):
+        return Like.objects.all()
+    
+    def perform_create(self, serializer):
+        
+        pk = self.kwargs['pk']
+        post_item = UserPost.objects.get(pk=pk)
+        requested_user = self.request.user
+        
+        user_like = Like.objects.filter(parent_post=post_item, liker=requested_user)
+        if user_like.exists():
+            post_item.likes_count -= 1
+            user_like.delete()
+        else:
+            post_item.likes_count += 1
+            post_item.save()
+            serializer.save(parent_post=post_item, liker=requested_user)
+            
+        
+            
