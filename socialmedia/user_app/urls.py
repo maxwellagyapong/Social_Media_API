f95e6e87c409d2728d8806cb1de1addab0127964
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-26 19:06:31 +0100

    Created enpoints for register and login

--- socialmedia/user_app/urls.py
+++ socialmedia/user_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import RegistrationView, LoginView
 
 urlpatterns = [
-    
+    path('register/', RegistrationView.as_view(), name='register-account'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
