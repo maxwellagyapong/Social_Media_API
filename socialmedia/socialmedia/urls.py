f7a3133d4bdebb77faac55190683daad3b05db36
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-26 18:33:53 +0100

    Created User serializer

--- socialmedia/socialmedia/urls.py
+++ socialmedia/socialmedia/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('social_app.urls')),
-    path('api-auth/', include('rest_framework.urls')),
 ]
