6381788fe1d3358c067f088b7748ff43a5390caa
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-26 18:47:33 +0100

    Set default authentication backend for JWT authentication

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -131,3 +131,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
