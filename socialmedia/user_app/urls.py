70e4f2d93d8ca9fde7e8d9a546714b221bc75b0f
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-27 17:37:30 +0100

    Configured enpoint for editting profile picture

--- socialmedia/user_app/urls.py
+++ socialmedia/user_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegistrationView, LoginView, LogoutView
+from .views import RegistrationView, LoginView, LogoutView, EditProfilePicView
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='register-account'),
+    path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('<int:pk>/profile-pic/', EditProfilePicView.as_view(), name='profile-pic-edit'),
 ]
